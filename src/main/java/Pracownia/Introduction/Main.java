52437199ff39526c827f475cecf0bf4665f036c6
s432754 <vlaosa@st.amu.edu.pl>
2018-10-11 11:25:29 +0200

    qwe

--- src/main/java/Pracownia/Introduction/Main.java
+++ src/main/java/Pracownia/Introduction/Main.java
@@ -1,8 +1,18 @@
 package Pracownia.Introduction;
 
-public class Main {
+import java.util.logging.Logger;
 
+public class Main {
+    static Logger log = Logger.getLogger("abc");
     public static void main(String [ ] args) {
+        log.info("message");
         System.out.println("Hello World");
     }
+
+
 }
+
+
+
+
+
